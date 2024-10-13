fba569ae8a967c8bdbce36a0927c6eae9e2116c1
Collinzy0451 <jacobcollinzy6@gmail.com>
2024-10-14 00:05:24 +0100

    backend workings

---
+++ backend/src/users/user.route.js
@@ -0,0 +1,148 @@
+const express = require('express');
+const User = require('./user.model');
+const { Schema } = require('mongoose');
+const router = express.Router();
+const generateToken = require('../middleware/generateToken');
+
+
+//Register endpoint
+router.post("/register", async (req, res) => {
+    try {
+        const {username, email, password} = req.body;
+        const user = new User({email, username, password});
+        await user.save();
+        res.status(201).send({message: "User registered successfully!"})
+    } catch (error) {
+        console.error("Error registering user", error);
+        res.status(500).send({message: "Error registering user", })
+
+    }
+})
+
+//login user endpoint
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(404).send({message: 'User not found'})
+        }
+        const isMatch = await user.comparePassword(password);
+        if(!isMatch) {
+            return res.status(401).send({message: 'Passwprd does not match'})
+        }
+
+        const token = await generateToken(user._id);
+
+        req.cookies('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None'
+        })
+
+        res.status(200).send({message: "Logged in successfully", token, user: {
+            _id: user._id,
+            email: user.email,
+            username: user.username,
+            profileImage: user.profileImage,
+            bio: user.bio,
+            profession: user.profession,
+            role: user.role, 
+        },
+     });
+    } catch (error) {
+        console.error("Error logged in user", error);
+        res.status(500).send({message: "Error logged in user", })
+    }
+
+})
+
+//logout func
+router.post('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.status(200).send({message: 'Logged out successfully'})
+})
+
+//delete a user
+router.delete('/users/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const user = await User.findByIdAndDelete(id);
+        if(!user) {
+            return res.status(404).send({message: 'User not found'})
+        }
+    } catch (error) {
+        console.error("Error deleting user", error);
+        res.status(500).send({message: "Error deleting user", })
+    }
+})
+
+//get all users
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({}, 'id email role').sort({createdAt: -1});
+        res.status(200).send(users)
+    } catch (error) {
+        console.error("Error fetching user", error);
+        res.status(500).send({message: "Error fetching user", });
+    }
+})
+
+//update user role
+router.put('/users/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {role} = req.body;
+        const user = await User.findByIdAndUpdate(id, {role}, {new: true});
+        if (!user) {
+            return res.status(404).send({message: 'User not found'})
+        }
+    } catch (error) {
+        console.error("Error updating user role", error);
+        res.status(500).send({message: "Error updating user role", });
+    }
+})
+
+//edit or update profile
+router.patch('/edit-profile', async (req, res) => {
+    try {
+        const {userId, username, profileImage, bio, profession} = req.body;
+        if (!userId){
+            return res.status(400).send({message: 'User ID is required'})
+        }
+        const user = await User.findBy.Id(userId);
+        
+        if(!user) {
+            return res.status(400).send({message: 'User not found'})
+        }
+
+        //update profile
+        if(username !== undefined) user.username = username;
+        if(profileImage !== undefined) user.profileImagee = profileImage;
+        if(bio !== undefined) user.bio = bio;
+        if(profession !== undefined) user.profession = profession;
+
+        await user.save();
+        res.status(200).send({
+            message: "Profile updated successfully",
+            user: {
+                _id: user._id,
+                email: user.email,
+                username: user.username,
+                role: user.role,
+                profileImage: user.profileImage,
+                bio: user.bio,
+                profession: user.profession,
+                role: user.role,    
+            },
+        });
+
+
+    } catch (error) {
+        console.error("Error updating user profile ", error);
+        res.status(500).send({message: "Error updating user profile", });
+    }
+})
+
+module.exports = router;
